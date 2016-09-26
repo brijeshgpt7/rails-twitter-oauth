80f6fb59ccb621841f324496168d96aef4647495
Brijesh <brijeshgpt7@gmail.com>
2016-09-26 18:34:13 +0530

    app level config

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,46 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :user_signed_in?
+  helper_method :correct_user?
+
+  private
+
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.omniauth_provider_key
+      config.consumer_secret =  Rails.application.secrets.omniauth_provider_secret
+      config.access_token = session[:access_token_key]
+      config.access_token_secret = session[:access_token_secret]
+    end
+  end
+
+  def current_user
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue Exception => e
+      nil
+    end
+  end
+
+  def user_signed_in?
+    return true if current_user
+  end
+
+  def correct_user?
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url, :alert => "Access denied."
+    end
+  end
+
+  def authenticate_user!
+    if !current_user
+      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+    end
+  end
+
+end
